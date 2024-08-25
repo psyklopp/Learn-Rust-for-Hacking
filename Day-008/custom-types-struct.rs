cae78a5abffa9a5cfbd8b6a8da932f2484713c3a
psyklopp <d3zible@gmail.com>
2024-08-25 15:15:10 +0200

    day 09 but added some code for day 08

--- Day-008/custom-types-struct.rs
+++ Day-008/custom-types-struct.rs
@@ -32,7 +32,7 @@ struct Rectangle {
 }
 
 fn display_point(point:Point) -> () {
-    println!("The coordinates are: \nx: {}, y: {}", point.x, point.y);
+    println!("The coordinates are: \n x: {}, y: {}", point.x, point.y);
 }
 
 fn main() {
@@ -43,9 +43,48 @@ fn main() {
     println!("{:?}", addy);
 
     let first_point = Point{x: 4.5, y: 2.5};
-    //let second_point = Point{x: 7.5, y: 1.5};
-    // we can use the concept of a line consisting of 2 points x and y on a 2D graph
-    let second_point = Point{x: 7.5, ..first_point};
+    let second_point = Point{x: 7.5, y: 1.5};
+    // we can also use the concept of a straight line on x-axis, consisting of 2 points x and y on a 2D graph
+    // let second_point = Point{x: 7.5, ..first_point}; --> the point only moves along the x-axis
     display_point(first_point);
     display_point(second_point);
+    
+    // Below from Rust by Example   
+
+    // Instantiate a `Point`
+    let point: Point = Point { x: 10.3, y: 0.4 };
+    let another_point: Point = Point { x: 5.2, y: 0.2 };
+
+    // Access the fields of the point
+    println!("point coordinates: ({}, {})", point.x, point.y);
+
+    let bottom_right = Point { x: 5.2, ..another_point };
+
+    // `bottom_right.y` will be the same as `point.y` because we used that field
+    // from `point`
+    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
+
+    // Destructure the point using a `let` binding
+    let Point { x: left_edge, y: top_edge } = point;
+    println!("left-edge : {}, top_edge : {}", left_edge, top_edge);
+
+    let _rectangle = Rectangle {
+        // struct instantiation is an expression too
+        top_left: Point { x: left_edge, y: top_edge },
+        bottom_right: bottom_right,
+    };
+
+    // Instantiate a unit struct
+    let _unit = Unit;
+
+    // Instantiate a tuple struct
+    let pair = Pair(1, 0.1);
+
+    // Access the fields of a tuple struct
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
+
+    // Destructure a tuple struct
+    let Pair(integer, decimal) = pair;
+
+    println!("pair contains {:?} and {:?}", integer, decimal);
 }
