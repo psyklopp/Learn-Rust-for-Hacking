684c9b559111b4c2bab780874bf44e510d08d3fb
psyklopp <d3zible@gmail.com>
2024-08-29 21:26:03 +0200

    day 12 - custom types - static and const

---
+++ Day-012/custom-types-constants.rs
@@ -0,0 +1,23 @@
+// Globals are declared outside all other scopes.
+static LANGUAGE: &str = "Rust";
+const THRESHOLD: i32 = 10;
+
+fn is_big(n: i32) -> bool {
+    // Access constant in some function
+    n > THRESHOLD
+}
+
+fn main() {
+    let n = 16;
+
+    // Access constant in the main thread
+    println!("This is {}", LANGUAGE);
+    println!("The threshold is {}", THRESHOLD);
+    println!("{} is {}", n, if is_big(n) { "big" } else { "small" });
+
+    // Error!1 if you uncomment the following 2
+    // Cannot modify a `const`, also accessing or modifying a mutable static variable is unsafe.
+    
+    /#// THRESHOLD = 5;
+    // LANGUAGE = "Java";
+}
