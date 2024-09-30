e75c1884994d60b32b2631d5e219996803c7ae22
psyklopp <d3zible@gmail.com>
2024-09-30 20:24:25 +0200

    guessing game - init

---
+++ day-023/guessing_game/src/main.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    println!("Please input your guess: ");
+
+    let mut guess = String::new();
+
+    let some_enum = io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("Your guess: {}", guess);
+}
